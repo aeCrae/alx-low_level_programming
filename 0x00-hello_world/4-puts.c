5fa14e88362aeb4325d758f53f36940013ed91f3
Abraham Oladayiye <adeyemiola01@gmail.com>
2022-07-04 16:40:13 +0000

    Script to print required text with puts added

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
-/**
- * main - entry point
- * 
- *Return: 0 after printing the function
- */
-int main(void)
-{puts("/"Programming is like building a multilingual puzzle");
+#include <string.h>
+
+int main(void)/*Entry Point*/
+{
+	char var_first[55];
+
+	strcpy(var_first, "\"Programming is like building a multilingual puzzle\n");
+	puts(var_first);
+
 	return (0);
 }
