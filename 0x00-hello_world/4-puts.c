993f81cfd4eda1c356ca3f1022a99450009919b4
Abraham Oladayiye <adeyemiola01@gmail.com>
2022-07-04 16:50:20 +0000

    Tahnks

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,7 +5,7 @@ int main(void) /* Entry Point of program */
 {
 	char var_first[55];
 
-	strcpy(var_first, "\"Programming is like building a multilingual puzzle\n");
+	strcpy(var_first, "\"Programming is like building a multilingual puzzle");
 	puts(var_first);
 
 	return (0);
