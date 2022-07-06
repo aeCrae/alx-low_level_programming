5cd1c37f8a98ac59f9236a3cadf639b43aa825fb
Abraham Oladayiye <adeyemiola01@gmail.com>
2022-07-06 16:01:02 +0000

    Hot fix 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,9 +8,11 @@ void print_alphabet_x10(void)
 	int count;
 
 	for (count = 1; count <= 10; count++)
+	{
 		for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 		{
 			_putchar(alphabet);
 		}
 		_putchar('\n');
+	}
 }
