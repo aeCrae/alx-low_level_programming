1fc77c65d0216ad2d2b8e63ec7bc8e8b525100e2
Abraham Oladayiye <adeyemiola01@gmail.com>
2022-07-07 03:38:13 +0000

    Files added are sign and alpha lower or upper case checker

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_sign -> a function to tell positive from
+ * negative numbers
+ * @n: the parameter to be checked
+ * Return: values for return check
+ */
+int print_sign(int n)
+{
+	int i;
+
+	if (i < 0)
+		return (-1);
+	else if (i == 0)
+		return (0);
+	else
+		return (1);
+}
