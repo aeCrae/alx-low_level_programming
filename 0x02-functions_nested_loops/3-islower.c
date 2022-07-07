ad2e4210641b6e0e86594808519487d8f1169d8a
Abraham Oladayiye <adeyemiola01@gmail.com>
2022-07-07 02:53:17 +0000

    Danke in German

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _islower -> checks for lower case letters
+ * @c: success always 0
+ * Return: the argument passed into islower
+ */
+int _islower(int c)
+{
+	return (c >= 'a';
+			&& <= 'z');
+}
