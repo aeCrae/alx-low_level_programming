9e63f21f0e2d9850379fbcd812840e0e1ad0d2f5
Abraham Oladayiye <adeyemiola01@gmail.com>
2022-07-08 15:13:09 +0000

    Through the glitch, reach your peak

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * mul - this function multplies two integers
+ * @a: integer one
+ * @b: integer two
+ * Return: the answer
+ */
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+	return (c);
+}
