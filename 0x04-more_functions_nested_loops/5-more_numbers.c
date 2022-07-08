a5e3d028123ebe8d1205cbc85cdbed641ecbfd77
Abraham Oladayiye <adeyemiola01@gmail.com>
2022-07-08 16:13:39 +0000

    Bla bla black sheep, have you any wool

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * more_numbers - Abeg check the code
+ */
+ void more_numbers(void)
+{
+	int i;
+	int x = 1;
+
+	while (x <= 14)
+	{
+		for (i = 0; i <= 14; i++)
+		{
+			_putchar(i + '0');
+		}
+		_putchar('\n');
+	}
+}
