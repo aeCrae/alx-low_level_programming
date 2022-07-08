9cb1690a6f76711661796a699a4bbd4239ca57eb
Abraham Oladayiye <adeyemiola01@gmail.com>
2022-07-08 13:21:47 +0000

    donestag

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _isdigit - to check for digits from 0 to 9
+ * @c: the parameter or digit to be checked
+ * loaded from 1-main.c
+ * Return: value to show if parameter digit is a * nywhere from 0 - 9.
+ */
+int _isdigit(int c)
+{
+	return (c >= 0 && c <= 9);
+}
