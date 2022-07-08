1e919896daae8a86dbadb535c99c1fde46ab955f
Abraham Oladayiye <adeyemiola01@gmail.com>
2022-07-08 15:48:12 +0000

    Ist mein Vater und mein Mutter

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,17 +1,14 @@
 #include "main.h"
 
 /**
- * print_numbers - to print integers to the cons * ole
- * Return: success always 0.
+ * print_numbers - to print integers to the cons * olsuccess always 0.
  */
 
 void print_numbers(void)
 {
-	char i;
+	int i;
 
 	for (i = 0; i <= 9; i++)
-	{
 		_putchar(i + '0');
-	}
 	_putchar('\n');
 }
