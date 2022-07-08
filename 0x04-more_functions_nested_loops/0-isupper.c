36e8c7aaa4e605d32cb7178eaa5bbf121d921a81
Abraham Oladayiye <adeyemiola01@gmail.com>
2022-07-08 11:55:38 +0000

    is upper to check for uppercase alphabets done

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _isupper - checks if a letter is uppercase an * and
+ * @c: the parameter to be checked.
+ * Return: 0 for non and 1 for affirmative.
+ */
+int _isupper(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,2 @@
+In this folder or directory, we are expected to take more excercises on functions,
+so that we can master the concept of functions in the C programming language.
