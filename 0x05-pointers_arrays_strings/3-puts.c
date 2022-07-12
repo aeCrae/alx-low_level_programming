4b5edc088f6800ad24483c3a794677c4447d00f2
Abraham Oladayiye <adeyemiola01@gmail.com>
2022-07-12 22:27:15 +0000

    This one to print a string to stdout, I know not how

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _puts - writes a function that prints a string, follow
+ * @*str: the parameter
+ * @c:  the processing
+ *
+ * by a new line, to stdout
+ * Return: nothing.
+ */
+void _puts(char *str)
+{
+	char c = *str;
+	_putchar('c\n');
+}
