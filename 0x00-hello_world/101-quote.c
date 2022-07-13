5b0725d53f58c55b7e8877b7ffe9854d7d394628
Abraham Oladayiye <adeyemiola01@gmail.com>
2022-07-13 12:02:33 +0000

    quote problems

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - the entry point of the program
+ *
+ * Return: Success  always 0.
+ */
+int main(void)
+{
+	FILE *fp;
+	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+
+	fp = fopen("file.txt", "w");
+	fwrite(str, 1, sizeof(str), fp);
+	fclose(fp);
+	return (0);
+}
