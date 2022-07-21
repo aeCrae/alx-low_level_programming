2268b77a495740a8021b1627ede3841c1813bf35
Abraham Oladayiye <adeyemiola01@gmail.com>
2022-07-21 15:38:52 +0000

    Hot fix

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -7,15 +7,16 @@
  */
 int main(void)
 {
-char ch;
-for (ch = '0'; ch <= '9'; ch++)
-{
-	putchar(ch);
-}
-for (ch = 'a'; ch <= 'f'; ch++)
-{
-	putchar(ch);
-}
-putchar('\n');
-return (0);
+	char ch;
+	for (ch = '0'; ch <= '9'; ch++)
+	{
+		putchar(ch);
+	}
+	for (ch = 'a'; ch <= 'f'; ch++)
+	{
+		putchar(ch);
+	}
+
+	putchar('\n');
+	return (0);
 }
