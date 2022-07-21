b20a4adb8c41efd0c23036b49e9cfd045cf10fd8
Abraham Oladayiye <adeyemiola01@gmail.com>
2022-07-21 15:54:18 +0000

    Hot Fix

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -2,7 +2,7 @@
 /**
  * main - main block
  * Description: prints all single digit numbers of base 1
- * starting from 0,followed by a new line 
+ * starting from 0,followed by a new line
  * Return: 0
  */
 int main(void)
@@ -10,7 +10,7 @@ int main(void)
 	int c = 0;
 	int boy = 48;
 
-	while(c < 10)
+	while (c < 10)
 	{
 		c += boy;
 		putchar(c);
