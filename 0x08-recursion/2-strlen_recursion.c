23c78bcc2fad6f225d9a914edd6c511854f3db11
Abraham Oladayiye <adeyemiola01@gmail.com>
2022-07-22 09:02:20 +0000

    For the eagle, the sky is only a run way, it converses with the mountain

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - It's a function that returns the
+ * length of a string passed in as parameter.
+ * @s: Carries the string in.
+ * Return: returns the string length.
+ */
+int _strlen_recursion(char *s)
+{
+	int len = 0;
+	if (*s == '\0')
+		return (0);
+	s++;
+	return (1 + _strlen_recursion(s));
+}
