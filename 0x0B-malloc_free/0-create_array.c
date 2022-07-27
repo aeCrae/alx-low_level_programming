1804130a3867cfcf6f059d777105f5b8bf4beebb
Abraham Oladayiye <adeyemiola01@gmail.com>
2022-07-27 00:16:56 +0000

    Them

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,8 @@
+#include "main.h"
+/**
+ * create_array - a function to create an array of chars
+ * @size: an argument which sends the arrays size
+ * @c: another argument, not sure yet.
+ * Return: null if size is 0, returns a pointer to the arry
+ */
+char *create_array(unsigned int size, char c);
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,2 @@
+This directory contains c source codes for priacticing and
+understanding the concept of memory alloction. Especially automatic and dynamic memory allocation. For the effectiveness of code in run time especially.
