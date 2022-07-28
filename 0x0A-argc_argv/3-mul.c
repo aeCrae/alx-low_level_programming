947cb4aaa6bc3f073a74277c0441fe934e784383
Abraham Oladayiye <adeyemiola01@gmail.com>
2022-07-28 11:28:27 +0000

    To be faithful, loyal and honest

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+/**
+ * main - this is the entry point of the program
+ * @argc: counts the number of commandline arguments'
+ * @argv: store the content and is the index of arguments
+ * Return: success always returns 0.
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+	int k;
+	int mul = 1;
+
+	for (i = 1; i < argc; i++)
+	{
+		mul = mul * atoi(argv[i]);
+		/* The function atoi here converts argv[ith] item to int*/
+	}
+	/* since it was declared from the function as a char */
+	printf("%d\n", mul);
+	return (0);
+}
