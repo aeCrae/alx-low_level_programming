97b5b8746f760a2d2a33f0d1c2a73bfda31a6ed7
Abraham Oladayiye <adeyemiola01@gmail.com>
2022-07-28 10:39:43 +0000

    Road to stardom

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - This is the entry point of the program
+ * @argc: This is the number of command line arguments passed
+ * @argv: This is the indices and stores the actual arguments
+ * Return: success always returns 0.
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
