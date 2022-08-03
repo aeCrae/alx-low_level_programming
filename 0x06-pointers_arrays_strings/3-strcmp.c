cedcb604655d2ff00d3b251a45d786b981bc436d
Abraham Oladayiye <adeyemiola01@gmail.com>
2022-08-03 23:31:06 +0000

    Hula Baluu

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strcmp - A function to compare
+ * @s1: string one
+ *@s2: string two
+ *Return: an integer
+ */
+int _strcmp(char *s1, char *s2)
+{
+	return (strcmp(s1, s2));
+}
