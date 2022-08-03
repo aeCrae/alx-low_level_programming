a571ef53c9d9ba3ce8bffef7271f794e0acbabf3
Abraham Oladayiye <adeyemiola01@gmail.com>
2022-08-03 22:51:37 +0000

    Hot fix

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strncat - function to conct. two stirngs with n bytes
+ * @dest: destination string
+ * @src: the source string
+ * @n: the byte limit.
+ * Return: success always 0.
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	strncat (dest, src, n);
+	return (dest);
+}
