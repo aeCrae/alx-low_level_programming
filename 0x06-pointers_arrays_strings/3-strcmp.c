947349ed0233b9b6945671902a7154822645cf6a
Abraham <adeyemiola01>
2022-08-18 13:41:00 +0000

    dear Abraham, please believe in yourself

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -4,10 +4,23 @@
 /**
  * _strcmp - A function to compare
  * @s1: string one
- *@s2: string two
- *Return: an integer
+ * @s2: string two
+ * Return: an integer
  */
 int _strcmp(char *s1, char *s2)
 {
-	return (strcmp(s1, s2));
+	int i;
+
+	for (i = 0; s1[i] != '\0'; s1++)
+	{
+		for (i = 0; s2[i] != '\0'; s2++)
+		{
+			if (s1[i] == s2[i])
+				return (s1[i] - s2[i]);
+			else if (s1[i] > s2[i])
+				return (s1[i] - s2[i]);
+			else
+				return (s1[i] - s2[i]);
+		}
+	}
 }
